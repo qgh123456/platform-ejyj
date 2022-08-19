bf54c719f0d895f09081944416343b864bfc0509
qiguohui <qiguohui@senses-ai.com>
2022-08-19 21:34:48 +0800

    初始化

---
+++ ejyj-service/ejyj-common/ejyj-common-core/src/main/java/com/atqgh/common/utils/ResultObj.java
@@ -0,0 +1,123 @@
+package com.atqgh.common.utils;
+
+import com.atqgh.common.enums.ResultStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.io.Serializable;
+
+/**
+ * 返回数据.
+ *
+ *
+ * @author Mubai
+ */
+public final class ResultObj<T> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer code;
+
+    private String msg;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    private ResultObj(Integer code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    /**
+     * 成功.
+     * @param data 数据
+     * @param <T> 泛型
+     * @return 结果
+     */
+    public static <T> ResultObj success(T data) {
+
+        return new ResultObj(ResultStatus.SUCCESS.getCode(), ResultStatus.SUCCESS.getMessage(), data);
+    }
+
+    /**
+     * 成功.
+     * @param msg 消息
+     * @param data 数据
+     * @param <T> 泛型
+     * @return 结果
+     */
+    public static <T> ResultObj success(String msg, T data) {
+        return new ResultObj(ResultStatus.SUCCESS.getCode(), msg, data);
+    }
+
+
+    /**
+     * 错误参数.
+     * @return 结果
+     */
+    public static ResultObj error() {
+        return new ResultObj(ResultStatus.BUSINESS_REQUEST_FAILED.getCode(), ResultStatus.BUSINESS_REQUEST_FAILED.getMessage(), null);
+    }
+
+    /**
+     * 错误参数.
+     * @param msg 消息
+     * @return 结果
+     */
+    public static ResultObj error(String msg) {
+        return error(ResultStatus.BUSINESS_REQUEST_FAILED.getCode(), msg, null);
+    }
+
+    /**
+     * 错误结果.
+     * @param code 编码
+     * @param msg 消息
+     * @return 结果
+     */
+    public static ResultObj error(int code, String msg) {
+
+        return error(ResultStatus.BUSINESS_REQUEST_FAILED.getCode(), msg, null);
+    }
+
+    /**
+     * 错误结果.
+     * @param code 编码
+     * @param msg 消息
+     * @param data 数据
+     * @param <T> 泛型
+     * @return 结果
+     */
+    public static <T> ResultObj error(int code, String msg, T data) {
+
+        return new ResultObj(code, msg, data);
+    }
+
+//    /**
+//    * 利用fastjson进行反序列化.
+//    *
+//    * @param typeReference 转换的目标对象
+//    * @param <T> 目标对象类型
+//    * @return 结果
+//    */
+//    public <T> T getData(TypeReference<T> typeReference) {
+//
+//        T t = JSON.parseObject(data, typeReference);
+//        String key = "data";
+//        return getData(key, typeReference);
+//    }
+//
+//    /**
+//    * 利用fastjson进行反序列化.
+//    * @param key key
+//    * @param typeReference 类型
+//    * @param <T> 参数类型
+//    * @return 结果
+//    */
+//    public <T> T getData(String key, TypeReference<T> typeReference) {
+//        // 默认是map
+//        Object data = get(key);
+//        String jsonString = JSON.toJSONString(data);
+//        T t = JSON.parseObject(jsonString, typeReference);
+//        return t;
+//    }
+
+}
---
+++ ejyj-service/ejyj-system/ejyj-system-provider/src/main/java/com/atqgh/system/provider/SystemProviderApplication.java
@@ -0,0 +1,26 @@
+package com.atqgh.system.provider;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 系统资源服务启动类.
+ *
+ * @author Mubai
+ * @date 2022/6/30 11:00 上午
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+@MapperScan("com.atqgh.system.provider.mapper")
+public class SystemProviderApplication {
+
+    /**
+     * 系统启动类.
+     * @param args 请求参数
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SystemProviderApplication.class, args);
+    }
+}
